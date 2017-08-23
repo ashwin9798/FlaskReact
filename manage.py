68bbdbc79cbbc71fec11c82973a1d2b487078c9e
Ashwin Vivek <ashwin9798@gmail.com>
2017-08-23 21:13:05 +0800

    refactor with flask blueprint

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
 from flask_script import Manager
 
-from project import app,db
+from project import create_app, db
+from project.api.models import User
 
-
+app = create_app
 manager = Manager(app)
 
 @manager.command
