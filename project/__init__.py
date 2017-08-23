e3302dfdb2fb38512b0d28803e9bee8d6cc6d1f6
Ashwin Vivek <ashwin9798@gmail.com>
2017-08-23 20:23:48 +0800

    wire flask to postgres

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import datetime
+
 
 # instantiate the app
 app = Flask(__name__)
@@ -8,7 +10,8 @@ app = Flask(__name__)
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
-print(app.config)
+# instantiate postgres
+db = SQLAlchemy(app)
 
 # model
 class User(db.Model):
