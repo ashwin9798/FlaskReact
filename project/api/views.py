66a7c547a9194227683c0b37e4355c477b719215
Ashwin Vivek <ashwin9798@gmail.com>
2017-08-23 21:44:26 +0800

    post method

--- project/api/views.py
+++ project/api/views.py
@@ -1,4 +1,8 @@
 from flask import Blueprint, jsonify
+from sqlalchemy import exc
+
+from project.api.models import User
+from project import db
 
 users_blueprint = Blueprint('users', __name__)
 
@@ -8,3 +12,40 @@ def ping_pong():
         'status': 'success',
         'message': 'pong!'
     })
+
+@users_blueprint.route('/users', methods=['POST'])
+def add_user():
+    post_data = request.get_json()
+
+    if not post_data:
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_object), 400
+    username = post_data.get('username')
+    email = post_data.get('email')
+
+    try:
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            db.session.add(User(username=username, email=email, password=password))
+            db.session.commit()
+            response_object = {
+                'status': 'success',
+                'message': f'{email} was added!'
+            }
+            return jsonify(response_object), 201
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': 'Sorry. That email already exists.'
+            }
+
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_object), 400
