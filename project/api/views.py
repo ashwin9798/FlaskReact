40644b18aab694270b8c107412f080949d83a91e
Ashwin Vivek <ashwin9798@gmail.com>
2017-08-24 17:10:11 +0800

    get methods

--- project/api/views.py
+++ project/api/views.py
@@ -13,6 +13,7 @@ def ping_pong():
         'message': 'pong!'
     })
 
+#POST a user into the database
 @users_blueprint.route('/users', methods=['POST'])
 def add_user():
     post_data = request.get_json()
@@ -49,3 +50,50 @@ def add_user():
             'message': 'Invalid payload.'
         }
         return jsonify(response_object), 400
+
+#GET a single user from the database by id.
+@users_blueprint.route('/users/<user_id>', methods=['GET'])
+def get_single_user(user_id):
+    """Get single user details"""
+    response_object = {
+        'status': 'fail',
+        'message': 'User does not exist'
+    }
+    try:
+        user = User.query.filter_by(id=int(user_id)).first()
+        if not user:
+            return jsonify(response_object), 404
+        else:
+            response_object = {
+                'status': 'success',
+                'data': {
+                  'username': user.username,
+                  'email': user.email,
+                  'created_at': user.created_at
+                }
+            }
+            return jsonify(response_object), 200
+    except ValueError:
+        return jsonify(response_object), 404
+
+#GET all users in the database.
+@users_blueprint.route('/users', methods=['GET'])
+def get_all_users():
+    """Get all users"""
+    users = User.query.all()
+    users_list = []
+    for user in users:
+        user_object = {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'created_at': user.created_at
+        }
+        users_list.append(user_object)
+    response_object = {
+        'status': 'success',
+        'data': {
+          'users': users_list
+        }
+    }
+    return jsonify(response_object), 200
