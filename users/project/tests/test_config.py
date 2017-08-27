6cb46b34dcb1cb54f4cca4813d3d6aecaf6514b1
Ashwin Vivek <ashwin9798@gmail.com>
2017-08-27 14:59:20 +0800

    add code coverage

--- users/project/tests/test_config.py
+++ users/project/tests/test_config.py
@@ -5,6 +5,8 @@ from flask_testing import TestCase
 
 from project import create_app
 
+import os
+
 app = create_app()
 
 
@@ -19,7 +21,7 @@ class TestDevelopmentConfig(TestCase):
         self.assertFalse(current_app is None)
         self.assertTrue(
             app.config['SQLALCHEMY_DATABASE_URI'] ==
-            'postgres://postgres:postgres@users-db:5432/users_dev'
+            os.environ.get('DATABASE_URL')
         )
 
 
@@ -35,7 +37,7 @@ class TestTestingConfig(TestCase):
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
         self.assertTrue(
             app.config['SQLALCHEMY_DATABASE_URI'] ==
-            'postgres://postgres:postgres@users-db:5432/users_test'
+            os.environ.get('DATABASE_URL')
         )
 
 
