491a4d657706b6336b5a7c5c638733d1efff516a
Ashwin Vivek <ashwin9798@gmail.com>
2017-09-09 22:19:59 +0800

    display response

--- client/src/index.js
+++ client/src/index.js
@@ -6,13 +6,18 @@ import axios from 'axios';
 const App = () => {
   constructor() {
     super()
-    this.getUsers()
+    this.state = {
+      users: []
+    }
+  }
+  componentDidMount() {
+    this.getUsers();
   }
 
   getUsers() {
     axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
-      .then((res) => { console.log(res); })
-      .catch((err) => { console.log(err); })
+    .then((res) => { this.setState({ users: res.data.data.users }); })
+    .catch((err) => { console.log(err); })
   }
 
   return (
@@ -22,6 +27,11 @@ const App = () => {
           <br/>
           <h1>All Users</h1>
           <hr/><br/>
+          {
+            this.state.users.map((user) => {
+              return <h4 key={user.id} className="well"><strong>{ user.username }</strong> - <em>{user.created_at}</em></h4>
+            })
+          }
         </div>
       </div>
     </div>
